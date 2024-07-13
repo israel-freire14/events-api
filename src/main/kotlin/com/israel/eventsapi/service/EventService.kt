ef962d93eb139a6bca766acf0ee712e9a746a96e
israel-freire14 <israelfreire999@gmail.com>
2024-07-13 02:24:09 -0300

    feat: creating EventService

---
+++ src/main/kotlin/com/israel/eventsapi/service/EventService.kt
@@ -0,0 +1,18 @@
+package com.israel.eventsapi.service
+
+
+import com.israel.eventsapi.domain.Event.EventRequestDTO
+import org.springframework.stereotype.Service
+import org.springframework.web.multipart.MultipartFile
+
+
+@Service
+class EventService {
+
+    fun createEvent(data: EventRequestDTO) {
+       var imgUrl: String? = null
+
+    }
+    
+}
+
